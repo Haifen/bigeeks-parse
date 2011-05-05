712ee0536daf39da30ccfc7bc0befe52b7942ae0
Robin Kauffman <robink@robin-kauffmans-macbook-pro.local>
2011-05-04 21:57:56 -0700

    Added functions to write data to a file

--- src/bigeeks/datagen.clj
+++ src/bigeeks/datagen.clj
@@ -1,4 +1,9 @@
-(ns bigeeks.datagen)
+(ns bigeeks.datagen
+	(:require [clojure.core :as c]
+		  [clojure.contrib.seq :as s]
+		  [clojure.contrib.io :as io]
+		  [clojure.contrib.string :as string]
+		  [clj-http.client :as http]))
 
 (defn mk-data
 	"(list [0 1 2 ... n] [0 1 2 ... n] ...)"
@@ -6,7 +11,15 @@
 	(let [mk-rand #(int (* (+ (rand) (rand)) %))
 	      mk-cols #(map mk-rand (range cols))]
 	      (repeatedly mk-cols)))
-	     
-(defn mk-cols
-	[cols]
-	(map #(int (* (+ (rand) (rand)) %)) (range cols)))
+
+(defn dumptofile
+	[filename rows cols]
+	(io/write-lines filename (map #(string/join ", " %) (take rows (mk-data cols)))))
+
+(defn dumptofile2
+	[filename rows cols]
+	(loop [myline (take rows (mk-data cols))]
+		(when (first myline)
+			(println (string/join ", " (first myline)))
+		(recur (next myline)))))
+			
